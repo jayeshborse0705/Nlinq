4a14f9504d6a97854990cce23396326083ab945c
jayesh <jayeshborse0705@gmail.com>
2022-09-04 21:24:30 -0700

    [jayesh] create  linq

---
+++ LinqProject/LinqProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductReview productReview = new ProductReview();
+            productReview.AddProductReview();
+            productReview.DisplayList();
+            
+
+        }
+    }
+}
